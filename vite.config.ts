32974df686d5ae46851a040a78a95aac95a6a0fa
Wing Lee <contact@winglee.io>
2025-08-02 23:58:19 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,9 @@ export default defineConfig({
             projects: ['./tsconfig.json'],
         }),
         tailwindcss(),
-        tanstackStart(),
+        tanstackStart({
+            customViteReactPlugin: true,
+        }),
+        react(),
     ],
 });
